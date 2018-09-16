64144e575745af58d71ac0897fbdf37c17bf9986
Subeg7 <subeg7@gmail.com>
2018-09-17 01:01:43 +0545

    inputs categorized

--- src/calulator/Calulator.java
+++ src/calulator/Calulator.java
@@ -15,24 +15,42 @@ import java.util.Scanner;
  */
 public class Calulator {
 
-    Calulator calc  = new Calulator();
+    Calulator calc = new Calulator();
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String exp = "2*3+5/(6+7*5^2)";
+        int expInd = 0;
+        int expLength = 0;
+        
         
         //input the expression
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter a text: ");
+
 //        String input = reader.nextLine();
 //        System.out.println("THe input was " + input);
-
+        expLength = exp.length();
+        
         //instantiating Stack and Queue
         Stack opStack = new Stack();
         Queue queue = new Queue();
 
         //parsing the expression
-        System.out.println(isSmaller('-','+') );
+        while(expLength!=0){
+            char item = exp.charAt(expInd);
+            if(isNumber(item)){
+            }else if(isOperator(item)){
+
+            }
+            
+            expInd++;
+            expLength--;
+        }
+        
+        
         reader.close();
     }
 
@@ -47,18 +65,35 @@ public class Calulator {
         order.add('*');
         order.add('/');
         order.add(' ');
-        order.add('^');        
+        order.add('^');
         order.add(' ');
         order.add('(');
 
-        
         int inMain = order.indexOf(toCheck);
         int inCheck = order.indexOf(fromCheck);
-        
-        if(inCheck-inMain>1) return true;
-        else return false;
 
+        if (inCheck - inMain > 1) {
+            return true;
+        } else {
+            return false;
+        }
 
     }
 
+    public static boolean isNumber(char item){
+               if(item == '0' ||item == '1' ||item == '2' ||item == '3' ||item == '4' ||item == '5' ||item == '6' ||item == '7' ||item == '8' ||item == '9')
+                   return true;
+               else 
+                   return false;
+
+
+    }
+    
+    public static boolean isOperator(char item){
+        if(item == '(' ||item == ')' ||item == '-' ||item == '+' ||item == '*' ||item == '^' ||item == '/' )
+                   return true;
+               else 
+                   return false;
+    }
+        
 }
